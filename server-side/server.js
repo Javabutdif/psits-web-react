e162c90341dbcb8df2a7471346e566d6bbce9aad
Anton James Genabio <jamesgenabio31@gmail.com>
2024-06-01 20:51:05 +0800

    initial update

--- server-side/server.js
+++ server-side/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 // MongoDB connection
 mongoose
-  .connect("mongodb://localhost:27017/mernapp", {
+  .connect("mongodb://localhost:27017", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
