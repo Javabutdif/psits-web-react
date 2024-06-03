4193ed5dee5c84f7966d0ab51ec95a229bade7ba
Anton James Genabio <jamesgenabio31@gmail.com>
2024-06-03 20:50:56 +0800

    fix navigate

--- client-side/src/pages/Register.jsx
+++ client-side/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import BackendConnection from "../api/BackendApi";
 
 function Register() {
