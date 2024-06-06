fc8c7814255bcee28fcd385774cd3e08b9aa17bd
Anton James Genabio <jamesgenabio31@gmail.com>
2024-06-07 02:49:14 +0800

    added membership tabs

---
+++ client-side/src/Admin/MembershipRequest.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "../App.css";
+
+function MembershipRequest() {}
+
+export default MembershipRequest;
