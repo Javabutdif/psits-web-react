a317f6b3f65dcb07e6c5d69f15858ba296cce956
Anton James Genabio <jamesgenabio31@gmail.com>
2024-06-07 17:55:42 +0800

    back to original deployment

--- client-side/src/api/BackendApi.jsx
+++ client-side/src/api/BackendApi.jsx
@@ -1,6 +1,6 @@
 import react from "react";
 
 function RenderBackendConnection() {
-  return "https://psits-web-react.railway.internal";
+  return "https://psits-web-react-production.up.railway.app";
 }
 export default RenderBackendConnection;
