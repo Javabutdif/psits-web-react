b0d55a5f128ab789147845bf1408db7a8c1fa2c2
Anton James Genabio <jamesgenabio31@gmail.com>
2024-06-10 22:36:13 +0800

    initial commit

--- client-side/src/pages/Home.jsx
+++ client-side/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import $ from "jquery";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import home from "../assets/images/home.png";
