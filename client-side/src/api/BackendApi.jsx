1ff6aef5f3fe802e15defef9318c01a666ce14a5
AdrianeDriane <dilaoaparre@gmail.com>
2024-06-13 07:26:52 +0800

    refactor [Adriane]: comment localhost and server url

--- client-side/src/api/BackendApi.jsx
+++ client-side/src/api/BackendApi.jsx
@@ -2,5 +2,7 @@ import react from "react";
 
 function RenderBackendConnection() {
   return "https://psits-web-react-production.up.railway.app";
+  // http://localhost:5000
+  // https://psits-web-react-production.up.railway.app
 }
 export default RenderBackendConnection;
