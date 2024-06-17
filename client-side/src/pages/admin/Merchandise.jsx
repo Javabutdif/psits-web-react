364c8db334267773972d88fe58a3f6aec156ad8e
Anton James Genabio <jamesgenabio31@gmail.com>
2024-06-18 04:01:29 +0800

    added [James] : added images prompt that this page is under construction xD

--- client-side/src/pages/admin/Merchandise.jsx
+++ client-side/src/pages/admin/Merchandise.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "../../App.css";
+import under from "../../assets/images/under.jfif";
 
 function Merchandise() {
-  return <h1 className="text-center mt-5">Merchandise</h1>;
+  return (
+    <div className="text-center ">
+      <h1 className="text-center mt-5">Merchandise</h1>
+      <img className="h-25 w-25" src={under} alt="Under" />
+    </div>
+  );
 }
 
 export default Merchandise;
--- client-side/src/pages/admin/MerchandiseOrders.jsx
+++ client-side/src/pages/admin/MerchandiseOrders.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../../App.css";
+import under from "../../assets/images/under.jfif";
 
 function MerchandiseOrders() {
-  return <h1 className="text-center mt-5">Orders</h1>;
+  return (
+    <div className="text-center ">
+      <h1 className="text-center mt-5">Orders</h1>
+      <img className="h-25 w-25" src={under} alt="Under" />
+    </div>
+  );
 }
 
 export default MerchandiseOrders;
