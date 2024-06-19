8c5ad047cd5e6768d8f22bf116d67834e9609276
AdrianeDriane <dilaoaparre@gmail.com>
2024-06-19 11:33:11 +0800

    feat [Adriane]: update merch by id backend

--- server-side/routes/merch.js
+++ server-side/routes/merch.js
@@ -70,4 +70,50 @@ router.get("/:_id", async (req, res) => {
   }
 });
 
+// UPDATE merch by id
+router.put("/:_id", async (req, res) => {
+  const {
+    name,
+    price,
+    stocks,
+    batch, //
+    description,
+    variation, //
+    size, //
+    start_date,
+    end_date,
+  } = req.body;
+
+  const id = req.params._id;
+
+  try {
+    const result = await Merch.updateOne(
+      { _id: id },
+      {
+        $set: {
+          name: name,
+          price: price,
+          stocks: stocks,
+          batch: batch, //
+          description: description,
+          variation: variation, //
+          size: size, //
+          start_date: start_date,
+          end_date: end_date,
+        },
+      }
+    );
+
+    if (result.matchedCount === 0) {
+      console.error("Merch not found");
+      return res.status(404).send("Merch not found");
+    }
+
+    res.status(200).send("Merch updated successfully");
+  } catch (error) {
+    console.error("Error updating student:", error.message);
+    res.status(500).send(error.message);
+  }
+});
+
 module.exports = router;
