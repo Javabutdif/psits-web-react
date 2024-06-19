0c427bfe8b0aa1eb705f474b6d8635397635b5b6
AdrianeDriane <dilaoaparre@gmail.com>
2024-06-19 11:16:30 +0800

    feat [Adriane]: get merch by id

--- server-side/routes/merch.js
+++ server-side/routes/merch.js
@@ -41,14 +41,32 @@ router.post("/", async (req, res) => {
   }
 });
 
-// GET list of Merches
+// GET list of merches
 router.get("/", async (req, res) => {
   try {
     const merches = await Merch.find();
     res.status(200).json(merches);
   } catch (error) {
-    console.error("Error fetching students:", error);
-    res.status(500).json("Internal Server Error");
+    console.error("Error fetching students:", error.message);
+    res.status(500).send(error.message);
+  }
+});
+
+// GET merch by id
+router.get("/:_id", async (req, res) => {
+  const id = req.params._id;
+
+  try {
+    const merch = await Merch.findById(id);
+
+    if (!merch) {
+      return res.status(404).json({ message: "Merch not found!" });
+    }
+
+    res.status(200).json(merch);
+  } catch (error) {
+    console.error("Error fetching merch:", error.message);
+    res.status(500).send(error.message);
   }
 });
 
