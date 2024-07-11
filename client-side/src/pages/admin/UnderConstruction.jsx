542411748153ad6d65753334100ca33f577b8268
AdrianeDriane <dilaoaparre@gmail.com>
2024-07-11 19:34:22 +0800

    feat [Adriane]: add bg overlay for expanded navbar

--- client-side/src/pages/admin/UnderConstruction.jsx
+++ client-side/src/pages/admin/UnderConstruction.jsx
@@ -4,7 +4,7 @@ import under from "../../assets/images/under.jfif";
 
 function UnderConstruction() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center flex-col">
+    <div className="h-full w-full flex items-center justify-center flex-col">
       <h1 className="text-center mt-5 text-xl">
         This place is under construction!
       </h1>
