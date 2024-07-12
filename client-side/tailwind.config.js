485234458352b907ac607d540b2dd2a0d25027b6
AdrianeDriane <dilaoaparre@gmail.com>
2024-07-13 05:14:27 +0800

    feat [Adriane]: segoe font, text colors and box shadow preset in tailwind config

--- client-side/tailwind.config.js
+++ client-side/tailwind.config.js
@@ -1,16 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'montserrat': ['Montserrat', 'sans-serif'],
+        montserrat: ["Montserrat", "sans-serif"],
+        segoe: [
+          "Segoe UI",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+      colors: {
+        blue: "#074873",
+      },
+      boxShadow: {
+        custom: "0px 0px 8px rgba(0, 0, 0, 0.24)",
       },
     },
   },
   plugins: [],
-}
-
+};
