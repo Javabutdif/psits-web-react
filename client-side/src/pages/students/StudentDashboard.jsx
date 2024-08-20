1dab2a02fd4d4a553fa0c7dcdb3ae650ca88682c
AdrianeDriane <dilaoaparre@gmail.com>
2024-08-21 01:03:33 +0800

    feat [Adriane]: fetch facebook posts data frontend

--- client-side/src/pages/students/StudentDashboard.jsx
+++ client-side/src/pages/students/StudentDashboard.jsx
@@ -1,24 +1,63 @@
-import React from "react";
-import { posts, events } from "../../@fakedb/data";
+import React, { useEffect, useState } from "react";
+import { events } from "../../@fakedb/data";
 import Posts from "./dashboard/Posts";
 import Events from "./dashboard/Events";
 import OperationHours from "./dashboard/OperationHours";
 import MembershipBanner from "./dashboard/Membership";
 import { getMembershipStatus } from "../../authentication/Authentication";
+import { InfinitySpin } from "react-loader-spinner";
 
 function StudentDashboard() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const membershipStatus = getMembershipStatus();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/facebook");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
 
   return (
     <main className="grid grid-cols-1 lg:grid-rows-[300px_250px_300px] gap-4 py-5 lg:grid-cols-3 xl:grid-cols-5">
       {/* Events Section */}
-      <div className="md:col-span-1 lg:col-span-1 lg:row-start-3 xl:row-start-1 xl:col-start-5 ">
+      <div className="md:col-span-1 lg:col-span-1 lg:row-start-3 xl:row-start-1 xl:col-start-5">
         <Events events={events} />
       </div>
 
       {/* Posts Section */}
       <div className="md:col-span-1 lg:col-span-2 xl:col-span-3">
-        <Posts posts={posts} />
+        {loading && (
+          <div className="flex justify-center items-center h-full">
+            <InfinitySpin
+              visible={true}
+              width="200"
+              color="#0d6efd"
+              ariaLabel="infinity-spin-loading"
+            />
+          </div>
+        )}
+        {error && (
+          <div className="text-red-500 text-center">
+            <p>Error: {error}</p>
+          </div>
+        )}
+        {!loading && !error && <Posts posts={posts} />}
       </div>
 
       {/* Operation Hours and Membership Banner */}
