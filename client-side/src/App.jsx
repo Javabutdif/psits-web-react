5943398b6a43c88fa970078a4e51628b1f4bd8c0
Anton James Genabio <jamesgenabio31@gmail.com>
2024-08-26 22:31:48 +0800

    feat [James]: admin register reroute

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -62,7 +62,7 @@ const App = () => {
           <Route path="/explore" element={<Explore />} />
           <Route path="/event" element={<Events />} />
           <Route path="/community" element={<Community />} />
-          <Route path="/register" element={<AdminRegister />} />
+          <Route path="/admin-register" element={<AdminRegister />} />
         </Route>
         <Route
           path="/admin/"
