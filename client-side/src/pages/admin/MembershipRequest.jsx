70d816d9d3e19b313383736d9951a36fc7219cde
Jims <116171287+Javabutdif@users.noreply.github.com>
2024-09-18 13:12:21 +0800

    Update MembershipRequest.jsx

--- client-side/src/pages/admin/MembershipRequest.jsx
+++ client-side/src/pages/admin/MembershipRequest.jsx
@@ -29,7 +29,8 @@ function MembershipRequest() {
   const [selectedRows, setSelectedRows] = useState([]);
   const [selectAll, setSelectAll] = useState(false);
 
-  console.log(conditionalPosition);
+  console.log("Data: " + conditionalPosition);
+  console.log("Data1: " + !conditionalPosition);
   const columns = [
     {
       key: "select",
