4c20cd9ab60b4e5a156c07ddf077200a5f11c9d4
Jims <116171287+Javabutdif@users.noreply.github.com>
2024-09-24 10:21:17 +0800

    Update admin.js

--- server-side/routes/admin.js
+++ server-side/routes/admin.js
@@ -12,33 +12,6 @@ const path = require("path");
 
 const router = express.Router();
 
-router.post("/admin", async (req, res) => {
-  const { id_number, email, password, name, course, year, position } = req.body;
-
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newAdmin = new Admin({
-      id_number,
-      email,
-      password: hashedPassword,
-      name,
-      course,
-      year,
-      position,
-    });
-    await newAdmin.save();
-
-    res.status(201).json("Registration successful");
-  } catch (error) {
-    if (error.code === 11000) {
-      res.status(400).json("Id number already exists");
-    } else {
-      console.error("Error saving new student:", error);
-      res.status(500).json("Internal Server Error");
-    }
-  }
-});
 
 router.put("/renew-student", async (req, res) => {
   try {
