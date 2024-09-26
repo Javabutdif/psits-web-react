c111ae55bcba13bb9a0604f2bb3ae612cbc7ee77
Jims <116171287+Javabutdif@users.noreply.github.com>
2024-09-25 19:13:10 -0700

    Update server.js

--- server-side/server.js
+++ server-side/server.js
@@ -18,7 +18,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: '*',
+    origin: "*", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
