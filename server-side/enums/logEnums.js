e381d391e6aba4fe4fd4d08ee472635775d67000
AdrianeDriane <dilaoaparre@gmail.com>
2024-10-03 23:05:54 +0800

    feat [Adriane]: enums for log actions

---
+++ server-side/enums/logEnums.js
@@ -0,0 +1,16 @@
+const LogActions = Object.freeze({
+  CREATE: "CREATE",
+  UPDATE: "UPDATE",
+  DELETE: "DELETE",
+  RENEW: "RENEW",
+  APPROVE: "APPROVE",
+  REJECT: "REJECT",
+  LOGIN: "LOGIN",
+  LOGOUT: "LOGOUT",
+  PUBLISH: "PUBLISH",
+  ARCHIVE: "ARCHIVE",
+  SOFT_DELETE: "SOFT_DELETE",
+  RESTORE: "RESTORE",
+});
+
+module.exports = { LogActions };
