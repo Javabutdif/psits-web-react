e85376aa31628d55fadec0c7e5aea494bda6da88
Jims <jamesgenabio31@gmail.com>
2024-11-08 17:02:21 +0800

    feat [James]: added authorization to the edit student

--- client-side/src/pages/admin/membership/AllMembers.jsx
+++ client-side/src/pages/admin/membership/AllMembers.jsx
@@ -16,306 +16,312 @@ import ChangePassword from "../../../components/ChangePassword";
 import { higherPosition } from "../../../components/tools/clientTools";
 
 const Membership = () => {
-  const [data, setData] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredData, setFilteredData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [studentIdToBeDeleted, setStudentIdToBeDeleted] = useState("");
-  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
-  const [memberToEdit, setMemberToEdit] = useState(null);
-  const [selectedRows, setSelectedRows] = useState([]);
-  const [selectAll, setSelectAll] = useState(false);
-  const [id, setId] = useState("");
-  const [viewChange, setViewChange] = useState(false);
+	const [data, setData] = useState([]);
+	const [searchQuery, setSearchQuery] = useState("");
+	const [filteredData, setFilteredData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [studentIdToBeDeleted, setStudentIdToBeDeleted] = useState("");
+	const [isEditModalVisible, setIsEditModalVisible] = useState(false);
+	const [memberToEdit, setMemberToEdit] = useState(null);
+	const [selectedRows, setSelectedRows] = useState([]);
+	const [selectAll, setSelectAll] = useState(false);
+	const [id, setId] = useState("");
+	const [viewChange, setViewChange] = useState(false);
+	const token = sessionStorage.getItem("Token");
 
-  const user = getInformationData();
+	const user = getInformationData();
 
-  const fetchData = async () => {
-    try {
-      const result = await membership();
-      setData(result);
-      setFilteredData(result);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching data: ", error);
-      setLoading(false);
-    }
-  };
+	const fetchData = async () => {
+		try {
+			const result = await membership();
+			setData(result);
+			setFilteredData(result);
+			setLoading(false);
+		} catch (error) {
+			console.error("Error fetching data: ", error);
+			setLoading(false);
+		}
+	};
 
-  const handleEditButtonClick = (row) => {
-    setMemberToEdit(row);
-    setIsEditModalVisible(true);
-  };
+	const handleEditButtonClick = (row) => {
+		setMemberToEdit(row);
+		setIsEditModalVisible(true);
+	};
 
-  const handleEditModalClose = () => {
-    setIsEditModalVisible(false);
-    setMemberToEdit(null);
-  };
+	const handleEditModalClose = () => {
+		setIsEditModalVisible(false);
+		setMemberToEdit(null);
+	};
 
-  const handleChangePassword = (id) => {
-    setId(id);
-    setViewChange(true);
-  };
-  const handleHideChangePassword = () => {
-    setViewChange(false);
-  };
+	const handleChangePassword = (id) => {
+		setId(id);
+		setViewChange(true);
+	};
+	const handleHideChangePassword = () => {
+		setViewChange(false);
+	};
 
-  const handleSaveEditedMember = async (updatedMember) => {
-    setIsLoading(true);
-    try {
-      const response = await axios.post(
-        `${backendConnection()}/api/editedStudent`,
-        updatedMember
-      );
-      console.log(response.data.message);
-      showToast("success", "Student updated successfully!");
-    } catch (error) {
-      console.error("Error updating student:", error);
-      showToast(
-        "error",
-        error.response?.data?.message ||
-          error.message ||
-          "An unexpected error occurred."
-      );
-    }
+	const handleSaveEditedMember = async (updatedMember) => {
+		setIsLoading(true);
+		try {
+			const response = await axios.post(
+				`${backendConnection()}/api/editedStudent`,
+				updatedMember,
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			);
+			console.log(response.data.message);
+			showToast("success", "Student updated successfully!");
+		} catch (error) {
+			console.error("Error updating student:", error);
+			showToast(
+				"error",
+				error.response?.data?.message ||
+					error.message ||
+					"An unexpected error occurred."
+			);
+		}
 
-    fetchData();
-    setIsLoading(false);
-  };
+		fetchData();
+		setIsLoading(false);
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, []);
 
-  useEffect(() => {
-    const filtered = data.filter((item) => {
-      const searchLower = searchQuery.toLowerCase();
-      return [
-        item.first_name,
-        item.middle_name,
-        item.last_name,
-        item.id_number,
-        item.email,
-        item.type,
-        item.course,
-        item.rfid,
-      ]
-        .map((value) => (value ? value.toString().toLowerCase() : ""))
-        .some((value) => value.includes(searchLower));
-    });
-    setFilteredData(filtered);
-  }, [searchQuery, data]);
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-  const showModal = (row) => {
-    setIsModalVisible(true);
-    setStudentIdToBeDeleted(row.id_number);
-  };
+	useEffect(() => {
+		const filtered = data.filter((item) => {
+			const searchLower = searchQuery.toLowerCase();
+			return [
+				item.first_name,
+				item.middle_name,
+				item.last_name,
+				item.id_number,
+				item.email,
+				item.type,
+				item.course,
+				item.rfid,
+			]
+				.map((value) => (value ? value.toString().toLowerCase() : ""))
+				.some((value) => value.includes(searchLower));
+		});
+		setFilteredData(filtered);
+	}, [searchQuery, data]);
 
-  const hideModal = () => {
-    setIsModalVisible(false);
-    setStudentIdToBeDeleted("");
-  };
+	const showModal = (row) => {
+		setIsModalVisible(true);
+		setStudentIdToBeDeleted(row.id_number);
+	};
 
-  const handleConfirmDeletion = async () => {
-    setIsLoading(true);
-    try {
-      const id_number = studentIdToBeDeleted;
+	const hideModal = () => {
+		setIsModalVisible(false);
+		setStudentIdToBeDeleted("");
+	};
 
-      if ((await studentDeletion(id_number, user.name)) === 200) {
-        const updatedData = data.filter(
-          (student) => student.id_number !== id_number
-        );
-        setData(updatedData);
-        setIsModalVisible(false);
-        showToast("success", "Student Deletion Successful!");
-      } else {
-        console.error("Failed to delete student");
-        showToast("error", "Student Deletion Failed! Please try again.");
-      }
-    } catch (error) {
-      console.error("Error deleting student:", error);
-      showToast("error", "Student Deletion Failed! Please try again.");
-    }
-    setIsLoading(false);
-  };
+	const handleConfirmDeletion = async () => {
+		setIsLoading(true);
+		try {
+			const id_number = studentIdToBeDeleted;
 
-  useEffect(() => {
-    if (selectAll) {
-      setSelectedRows(filteredData.map((item) => item.id_number));
-    } else {
-      setSelectedRows([]);
-    }
-  }, [selectAll, filteredData]);
+			if ((await studentDeletion(id_number, user.name)) === 200) {
+				const updatedData = data.filter(
+					(student) => student.id_number !== id_number
+				);
+				setData(updatedData);
+				setIsModalVisible(false);
+				showToast("success", "Student Deletion Successful!");
+			} else {
+				console.error("Failed to delete student");
+				showToast("error", "Student Deletion Failed! Please try again.");
+			}
+		} catch (error) {
+			console.error("Error deleting student:", error);
+			showToast("error", "Student Deletion Failed! Please try again.");
+		}
+		setIsLoading(false);
+	};
 
-  const handleRowSelection = (id_number) => {
-    setSelectedRows((prevSelectedRows) =>
-      prevSelectedRows.includes(id_number)
-        ? prevSelectedRows.filter((id) => id !== id_number)
-        : [...prevSelectedRows, id_number]
-    );
-  };
+	useEffect(() => {
+		if (selectAll) {
+			setSelectedRows(filteredData.map((item) => item.id_number));
+		} else {
+			setSelectedRows([]);
+		}
+	}, [selectAll, filteredData]);
 
-  const columns = [
-    {
-      key: "select",
-      label: (
-        <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-          <input
-            type="checkbox"
-            checked={selectAll}
-            onChange={() => setSelectAll(!selectAll)}
-          />
-        </motion.div>
-      ),
-      cell: (row) => (
-        <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-          <input
-            type="checkbox"
-            checked={selectedRows.includes(row.id_number)}
-            onChange={() => handleRowSelection(row.id_number)}
-          />
-        </motion.div>
-      ),
-    },
-    {
-      key: "name",
-      label: "Name",
-      selector: (row) =>
-        `${row.first_name} ${row.middle_name} ${row.last_name}`,
-      sortable: true,
-      cell: (row) => (
-        <div className="text-xs">
-          <div>{`${row.first_name} ${row.middle_name} ${row.last_name}`}</div>
-          <div className="text-gray-500">RFID: {row.rfid}</div>
-        </div>
-      ),
-    },
-    {
-      key: "id_number",
-      label: "Id ",
-      selector: (row) => row.id_number,
-      sortable: true,
-    },
-    {
-      key: "course",
-      label: "Course",
-      selector: (row) => row.course,
-      sortable: true,
-      cell: (row) => (
-        <div className="text-xs">
-          <div className="text-gray-500">
-            {row.course} - {row.year}
-          </div>
-        </div>
-      ),
-    },
-    {
-      key: "email",
-      label: "Email Account",
-      selector: (row) => row.email,
-      sortable: true,
-    },
-    {
-      key: "membership",
-      label: "Membership",
-      selector: (row) => row.membership,
-      sortable: true,
-      cell: (row) => (
-        <div className="text-center">
-          <span
-            className={`px-2 py-1 rounded text-xs ${
-              row.membership === "None"
-                ? "bg-gray-200 text-gray-800"
-                : row.membership === "Pending" || row.renew === "Pending"
-                ? "bg-yellow-200 text-yellow-800"
-                : "bg-green-200 text-green-800"
-            }`}
-          >
-            {row.membership === "None"
-              ? "None"
-              : row.membership === "Pending" || row.renew === "Pending"
-              ? row.renew === "Pending"
-                ? "Renewal"
-                : "Pending"
-              : "Active"}
-          </span>
-        </div>
-      ),
-    },
-    {
-      key: "actions",
-      label: "",
-      cell: (row) => (
-        <ButtonsComponent>
-          {higherPosition() && (
-            <FormButton
-              type="button"
-              text="Change Password"
-              onClick={() => handleChangePassword(row.id_number)}
-              icon={<i className="fas fa-edit" />} // Simple icon
-              styles="flex items-center space-x-2 bg-gray-200 text-gray-800 rounded-md px-3 py-1.5 transition duration-150 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400"
-              textClass="text-gray-800"
-              whileHover={{ scale: 1.02, opacity: 0.95 }}
-              whileTap={{ scale: 0.98, opacity: 0.9 }}
-            />
-          )}
+	const handleRowSelection = (id_number) => {
+		setSelectedRows((prevSelectedRows) =>
+			prevSelectedRows.includes(id_number)
+				? prevSelectedRows.filter((id) => id !== id_number)
+				: [...prevSelectedRows, id_number]
+		);
+	};
 
-          <FormButton
-            type="button"
-            text="Edit"
-            onClick={() => handleEditButtonClick(row)}
-            icon={<i className="fas fa-edit" />} // Simple icon
-            styles="flex items-center space-x-2 bg-gray-200 text-gray-800 rounded-md px-3 py-1.5 transition duration-150 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400"
-            textClass="text-gray-800" // Elegant text color
-            whileHover={{ scale: 1.02, opacity: 0.95 }}
-            whileTap={{ scale: 0.98, opacity: 0.9 }}
-          />
-          <FormButton
-            type="button"
-            text="Delete"
-            onClick={() => showModal(row)}
-            icon={<i className="fas fa-trash" />} // Simple icon
-            styles="flex items-center space-x-2 bg-gray-200 text-red-800 rounded-md px-3 py-1.5 transition duration-150 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-red-400"
-            textClass="text-red-800" // Elegant text color
-            whileHover={{ scale: 1.02, opacity: 0.95 }}
-            whileTap={{ scale: 0.98, opacity: 0.9 }}
-          />
-        </ButtonsComponent>
-      ),
-    },
-  ];
+	const columns = [
+		{
+			key: "select",
+			label: (
+				<motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+					<input
+						type="checkbox"
+						checked={selectAll}
+						onChange={() => setSelectAll(!selectAll)}
+					/>
+				</motion.div>
+			),
+			cell: (row) => (
+				<motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+					<input
+						type="checkbox"
+						checked={selectedRows.includes(row.id_number)}
+						onChange={() => handleRowSelection(row.id_number)}
+					/>
+				</motion.div>
+			),
+		},
+		{
+			key: "name",
+			label: "Name",
+			selector: (row) =>
+				`${row.first_name} ${row.middle_name} ${row.last_name}`,
+			sortable: true,
+			cell: (row) => (
+				<div className="text-xs">
+					<div>{`${row.first_name} ${row.middle_name} ${row.last_name}`}</div>
+					<div className="text-gray-500">RFID: {row.rfid}</div>
+				</div>
+			),
+		},
+		{
+			key: "id_number",
+			label: "Id ",
+			selector: (row) => row.id_number,
+			sortable: true,
+		},
+		{
+			key: "course",
+			label: "Course",
+			selector: (row) => row.course,
+			sortable: true,
+			cell: (row) => (
+				<div className="text-xs">
+					<div className="text-gray-500">
+						{row.course} - {row.year}
+					</div>
+				</div>
+			),
+		},
+		{
+			key: "email",
+			label: "Email Account",
+			selector: (row) => row.email,
+			sortable: true,
+		},
+		{
+			key: "membership",
+			label: "Membership",
+			selector: (row) => row.membership,
+			sortable: true,
+			cell: (row) => (
+				<div className="text-center">
+					<span
+						className={`px-2 py-1 rounded text-xs ${
+							row.membership === "None"
+								? "bg-gray-200 text-gray-800"
+								: row.membership === "Pending" || row.renew === "Pending"
+								? "bg-yellow-200 text-yellow-800"
+								: "bg-green-200 text-green-800"
+						}`}>
+						{row.membership === "None"
+							? "None"
+							: row.membership === "Pending" || row.renew === "Pending"
+							? row.renew === "Pending"
+								? "Renewal"
+								: "Pending"
+							: "Active"}
+					</span>
+				</div>
+			),
+		},
+		{
+			key: "actions",
+			label: "",
+			cell: (row) => (
+				<ButtonsComponent>
+					{higherPosition() && (
+						<FormButton
+							type="button"
+							text="Change Password"
+							onClick={() => handleChangePassword(row.id_number)}
+							icon={<i className="fas fa-edit" />} // Simple icon
+							styles="flex items-center space-x-2 bg-gray-200 text-gray-800 rounded-md px-3 py-1.5 transition duration-150 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400"
+							textClass="text-gray-800"
+							whileHover={{ scale: 1.02, opacity: 0.95 }}
+							whileTap={{ scale: 0.98, opacity: 0.9 }}
+						/>
+					)}
 
-  return (
-    <div className="">
-      <TableComponent columns={columns} data={filteredData} />
-      {isEditModalVisible && (
-        <EditMember
-          isVisible={isEditModalVisible}
-          onClose={handleEditModalClose}
-          studentData={memberToEdit}
-          onSave={handleSaveEditedMember}
-        />
-      )}
-      {isModalVisible && (
-        <ConfirmationModal
-          confirmType={ConfirmActionType.DELETION}
-          onCancel={hideModal}
-          onConfirm={handleConfirmDeletion}
-        />
-      )}
-      {viewChange && (
-        <>
-          <ChangePassword
-            id={id}
-            onCancel={handleHideChangePassword}
-            onSubmit={() => setViewChange(false)}
-          />
-        </>
-      )}
-    </div>
-  );
+					<FormButton
+						type="button"
+						text="Edit"
+						onClick={() => handleEditButtonClick(row)}
+						icon={<i className="fas fa-edit" />} // Simple icon
+						styles="flex items-center space-x-2 bg-gray-200 text-gray-800 rounded-md px-3 py-1.5 transition duration-150 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400"
+						textClass="text-gray-800" // Elegant text color
+						whileHover={{ scale: 1.02, opacity: 0.95 }}
+						whileTap={{ scale: 0.98, opacity: 0.9 }}
+					/>
+					<FormButton
+						type="button"
+						text="Delete"
+						onClick={() => showModal(row)}
+						icon={<i className="fas fa-trash" />} // Simple icon
+						styles="flex items-center space-x-2 bg-gray-200 text-red-800 rounded-md px-3 py-1.5 transition duration-150 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-red-400"
+						textClass="text-red-800" // Elegant text color
+						whileHover={{ scale: 1.02, opacity: 0.95 }}
+						whileTap={{ scale: 0.98, opacity: 0.9 }}
+					/>
+				</ButtonsComponent>
+			),
+		},
+	];
+
+	return (
+		<div className="">
+			<TableComponent columns={columns} data={filteredData} />
+			{isEditModalVisible && (
+				<EditMember
+					isVisible={isEditModalVisible}
+					onClose={handleEditModalClose}
+					studentData={memberToEdit}
+					onSave={handleSaveEditedMember}
+				/>
+			)}
+			{isModalVisible && (
+				<ConfirmationModal
+					confirmType={ConfirmActionType.DELETION}
+					onCancel={hideModal}
+					onConfirm={handleConfirmDeletion}
+				/>
+			)}
+			{viewChange && (
+				<>
+					<ChangePassword
+						id={id}
+						onCancel={handleHideChangePassword}
+						onSubmit={() => setViewChange(false)}
+					/>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default Membership;
