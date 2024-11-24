0140ada1c73abffe450e424518f8b903b96d47d8
Anton James Genabio <jamesgenabio31@gmail.com>
2024-11-24 23:19:23 +0800

    feat [James]: added christ

--- client-side/src/api/students.js
+++ client-side/src/api/students.js
@@ -1,7 +1,8 @@
 import "../App.css";
+import { showToast } from "../utils/alertHelper";
 import backendConnection from "./backendApi";
 import axios from "axios";
-import { showToast } from "../utils/alertHelper";
+
 const token = sessionStorage.getItem("Token");
 
 export const requestMembership = async (id_number) => {
@@ -56,11 +57,12 @@ export const getMembershipStatusStudents = async (id_number) => {
   } catch (error) {
     if (error.response && error.response.data) {
       console.log(error.response.data.message);
+      window.location.reload();
     } else {
       console.log(error.response.data.message);
+      window.location.reload();
     }
     console.error("Error:", error);
-   
   }
 };
 
