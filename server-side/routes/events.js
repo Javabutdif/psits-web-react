caefa77851118cc76ccd5433e4901858dbecc19a
Anton James Genabio <jamesgenabio31@gmail.com>
2025-01-23 23:59:14 +0800

    feat [James]: add backend

--- server-side/routes/events.js
+++ server-side/routes/events.js
@@ -4,4 +4,13 @@ const authenticateToken = require("../middlewares/authenticateToken");
 
 const router = express.Router();
 
+router.get("/", authenticateToken, async (req, res) => {
+  try {
+    const events = await Events.find();
+    return res.status(200).json({ data: events });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 module.exports = router;
