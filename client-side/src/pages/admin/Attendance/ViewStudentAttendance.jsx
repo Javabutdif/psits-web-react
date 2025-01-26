b0600d5e13dd2e2e0fb62d3a8c09eee38bf6b300
AdrianeDriane <dilaoaparre@gmail.com>
2025-01-26 22:40:34 +0800

    feat [Adriane]: t shirt size in attendance modal

--- client-side/src/pages/admin/Attendance/ViewStudentAttendance.jsx
+++ client-side/src/pages/admin/Attendance/ViewStudentAttendance.jsx
@@ -1,18 +1,14 @@
-
 import React, { useEffect } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
-const ViewStudentAttendance = ({ isVisible, onClose, studentData}) => {
-
+const ViewStudentAttendance = ({ isVisible, onClose, studentData }) => {
   if (!isVisible) {
     return null;
   }
 
-
   useEffect(() => {
     console.log("Modal Data:", studentData); // Debugging
   }, [studentData]);
-
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 ">
@@ -28,15 +24,27 @@ const ViewStudentAttendance = ({ isVisible, onClose, studentData}) => {
           <div className="flex flex-col justify-left gap-2 p-2 w-full sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl max-h-screen overflow-auto">
             <h2 className="text-xl font-semibold">Student Attendance</h2>
             <div className="flex flex-col justify-center ml-3 gap-2">
-              <p className="text-gray-800 text-sm"><strong>Name:</strong> {studentData.name}</p>
-              <p className="text-gray-800 text-sm"><strong>ID:</strong> {studentData.id_number}</p>
-              <p className="text-gray-800 text-sm"><strong>Course:</strong> {studentData.course}</p>
-              <p className="text-gray-800 text-sm"><strong>Year:</strong> {studentData.year}</p>
-              <p className="text-gray-800 text-sm"><strong>Campus:</strong> {studentData.campus}</p>
-              <p className="text-gray-800 text-sm"><strong>T-Shirt Size:</strong> {studentData.shirt_size}</p>
+              <p className="text-gray-800 text-sm">
+                <strong>Name:</strong> {studentData.name}
+              </p>
+              <p className="text-gray-800 text-sm">
+                <strong>ID:</strong> {studentData.id_number}
+              </p>
+              <p className="text-gray-800 text-sm">
+                <strong>Course:</strong> {studentData.course}
+              </p>
+              <p className="text-gray-800 text-sm">
+                <strong>Year:</strong> {studentData.year}
+              </p>
+              <p className="text-gray-800 text-sm">
+                <strong>Campus:</strong> {studentData.campus}
+              </p>
+              <p className="text-gray-800 text-sm">
+                <strong>T-Shirt Size:</strong> {studentData.shirtSize}
+              </p>
             </div>
           </div>
-          </div>
+        </div>
       </div>
     </div>
   );
