31667f4e27a33897b63421fe3f4a0d39cea2c492
AdrianeDriane <dilaoaparre@gmail.com>
2025-01-26 22:35:14 +0800

    feat [Adriane]: server side modify attendance api method

--- server-side/routes/events.js
+++ server-side/routes/events.js
@@ -35,4 +35,55 @@ router.get("/attendees/:id", authenticateToken, async (req, res) => {
   }
 });
 
+router.get(
+  "/attendance/:event_id/:id_number",
+  authenticateToken,
+  async (req, res) => {
+    const { event_id, id_number } = req.params;
+
+    try {
+      if (req.user?.role !== "Admin") {
+        return res.status(403).json({ message: "Access denied. Admins only." });
+      }
+
+      const eventId = new ObjectId(event_id);
+      const event = await Events.findOne({ eventId });
+
+      if (!event) {
+        return res.status(404).json({ message: "Event not found" });
+      }
+
+      const attendee = event.attendees.find(
+        (attendee) => attendee.id_number === id_number
+      );
+
+      if (!attendee) {
+        return res
+          .status(404)
+          .json({ message: "Attendee not found in this event" });
+      }
+
+      if (attendee.isAttended) {
+        return res.status(400).json({ message: "Attendance already recorded" });
+      }
+
+      attendee.isAttended = true;
+      attendee.attendDate = new Date();
+      attendee.confirmedBy = req.user?.name;
+
+      await event.save();
+
+      res.status(200).json({
+        message: "Attendance successfully recorded",
+        data: attendee,
+      });
+    } catch (error) {
+      console.error("Error marking attendance:", error);
+      res
+        .status(500)
+        .json({ message: "An error occurred while recording attendance" });
+    }
+  }
+);
+
 module.exports = router;
