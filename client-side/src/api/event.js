ac1986375c85995c2db43f2a67edaff93be25655
AdrianeDriane <dilaoaparre@gmail.com>
2025-02-23 15:17:04 +0800

    feat [Adriane]: client side raffleWinner method

--- client-side/src/api/event.js
+++ client-side/src/api/event.js
@@ -38,8 +38,6 @@ export const getAttendees = async (id) => {
         },
       }
     );
-    // console.log(response.data.data[0].attendees);
-    //console.log(response.data.data[0]);
     return {
       data: response.data.data[0],
       attendees: response.data.data[0].attendees,
@@ -68,7 +66,6 @@ export const markAsPresent = async (eventId, attendeeId, navigate) => {
         },
       }
     );
-    console.log("keypress");
     if (response.status === 200) {
       showToast("success", "Attendance successfully recorded!");
       navigate(`/admin/attendance/${eventId}`); // Navigate back after successful update
@@ -108,7 +105,6 @@ export const getEventCheck = async (eventId) => {
         },
       }
     );
-    console.log(response.data.data.limit);
     return response.data.data;
   } catch (error) {
     if (error.response && error.response.data) {
@@ -141,5 +137,24 @@ export const updateEventSettings = async (formData, eventId) => {
       console.log("error", "An error occurred");
       return false;
     }
+  }
+};
+
+export const raffleWinner = async (eventId, campus) => {
+  try {
+    const response = await axios.post(
+      `${backendConnection()}/api/events/raffle/${eventId}`,
+      { campus },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error selecting raffle winner:", error);
+    return false;
   }
 };
