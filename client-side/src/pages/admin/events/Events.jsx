13c595e5e2b5cb00e0c98da715e480c50c0018fd
Jan <larocojanlorenz@gmail.com>
2025-08-06 01:07:17 +0800

    feat[Laroco]: added student events session in qr code

--- client-side/src/pages/admin/events/Events.jsx
+++ client-side/src/pages/admin/events/Events.jsx
@@ -5,6 +5,8 @@ import { getEvents } from "../../../api/event";
 import { getInformationData } from "../../../authentication/Authentication";
 import { formattedDate } from "../../../components/tools/clientTools";
 import AddEvent from "./AddEvent";
+import { motion } from "framer-motion";
+
 
 function Events() {
   const [events, setEvent] = useState([]);
@@ -68,7 +70,16 @@ function Events() {
           {isAddEventModalOpen && <AddEvent handleClose={closeModal} />}
           {events &&
             events.map((event) => (
-              <div className="bg-white rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow duration-300 ">
+              <motion.div 
+              className="bg-white rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow duration-300 "
+               whileHover={{ 
+                  y: -4, // Moves up 4 pixels
+                  transition: { 
+                    type: "spring", 
+                    damping: 10, 
+                    stiffness: 300 
+                  }
+                }}>
                 <div className="relative w-full h-48 mb-3">
                   <img
                     src={event.eventImage[0]}
@@ -83,7 +94,7 @@ function Events() {
                   <p className="mb-3 text-[074873]">
                     {formattedDate(event.eventDate)}
                   </p>
-                  <div className="flex gap-1 items-center justify-center">
+                  <div className="flex gap-1 p-3 items-center justify-center">
                     <Link
                       to={`/admin/attendance/${event.eventId}`}
                       className={
@@ -127,7 +138,7 @@ function Events() {
                     )}
                   </div>
                 </div>
-              </div>
+              </motion.div>
             ))}
         </div>
       )}
